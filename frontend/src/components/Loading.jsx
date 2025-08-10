c2c1fa5f3ea453fb5d3f70ca51b5ecd6dfe09a8c
iamsayannath <sayannath441@gmail.com>
2025-08-11 04:22:10 +0530

    add spinner

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,2 +1,3 @@
 import React from 'react';
-export default function Loading() { return <div className="p-8">Loading...</div>; }
+import { RotateSpinLoader } from 'react-css-loaders';
+export default function Loading() { return <div className="p-8"><RotateSpinLoader /></div>; }
