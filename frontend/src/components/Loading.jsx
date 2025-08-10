6e485b00893c6f91c4fe8a7c0c6edc7ed96e7848
iamsayannath <sayannath441@gmail.com>
2025-08-11 04:28:14 +0530

    fix

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,3 +1,2 @@
 import React from 'react';
-import { RotateSpinLoader } from 'react-css-loaders';
-export default function Loading() { return <div className="p-8"><RotateSpinLoader /></div>; }
+export default function Loading() { return <div className="p-8">Loading...</div>; }
